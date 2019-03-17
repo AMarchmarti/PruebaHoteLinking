c1d84170d992df8df296a9f50cf6f22c714a7f93
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 10:38:31 +0100

    añadido titulo y mensaje

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -1,5 +1,6 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
-    
+    <h1>{{ $title }}</h1>
+    <p>Aquí encontrará diferentes ofertas que le puedan interesar, ya sean noches gratuitas, descuentos para hoteles, entre otros...</p>
 @endsection
