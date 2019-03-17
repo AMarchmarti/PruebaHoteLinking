7d9f6a0323fbc85e2e6379b1e11b7c7b037d3a67
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 10:38:16 +0100

    añadido login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,5 +1,9 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
-    
+  <div class="jumbotron text-center">
+    <h1>{{ $title }}</h1>
+    <p>Gracias por visitarnos, por favor introduce su email y contraseña para acceder. <br> 
+      En caso de no estar registrado por favor, le invitamos a que se registre.<br></p>
+      <p><a class="btn btn-primary btn-lg" href="/offers" role="button">Login</a> <a class="btn btn-succes btn-lg" href="/register" role="button">Register</a></p>
 @endsection
