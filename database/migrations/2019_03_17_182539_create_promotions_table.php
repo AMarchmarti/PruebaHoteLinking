2479d90a82176aa74faa7a472258efa02669fc28
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 20:10:57 +0100

    creacion de la tabla

---
+++ database/migrations/2019_03_17_182539_create_promotions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('offers_id');
+            $table->string('code');
+            $table->tinyInteger('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
