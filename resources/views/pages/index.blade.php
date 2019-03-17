c84a80b9a88b9b1ae6c44372d68d7cacc3734896
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 18:17:26 +0100

    pendiente de terminar principal

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="jumbotron text-center">
+    <h1>{{ $title }}</h1>
+    <p>Gracias por visitarnos, por favor introduzca su email y contraseña para acceder. <br> 
+      En caso de no estar registrado por favor, le invitamos a que se registre.<br></p>
+    
+    <!--Pendiente de ampliar-->
+@endsection
