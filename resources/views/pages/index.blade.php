cd39d375c31be63ae02125e010e1589cfc3627a9
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 19:22:05 +0100

    cerrar etiquetas

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,6 @@
     <h1>{{ $title }}</h1>
     <p>Gracias por visitarnos, por favor introduzca su email y contraseña para acceder. <br> 
       En caso de no estar registrado por favor, le invitamos a que se registre.<br></p>
-    
+  </div>
     <!--Pendiente de ampliar-->
 @endsection
