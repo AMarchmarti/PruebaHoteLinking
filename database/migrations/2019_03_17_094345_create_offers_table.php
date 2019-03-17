3d039f2d26ce6637c64da003b82f22bde42ef607
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-17 11:09:39 +0100

    tabla ofertas

---
+++ database/migrations/2019_03_17_094345_create_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Offers_name')->length(10)->nullable();
+            $table->mediumText('description');
+            $table->bigIncrements('prefix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
